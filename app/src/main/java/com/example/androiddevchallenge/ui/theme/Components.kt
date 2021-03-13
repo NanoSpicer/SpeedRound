f71f3e3af17658b5486549f180b799a9161ecda4
Miquel Àngel <miquelrc95@outlook.es>
2021-03-13 14:29:31 +0100

    Adds support for icon

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Components.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Components.kt
@@ -3,10 +3,12 @@ package com.example.androiddevchallenge.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -16,13 +18,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
-
+import com.example.androiddevchallenge.R
 
 
 @Composable
@@ -63,7 +65,8 @@ private fun MySootheTextField(
     defaultText: String = "",
     visualTransformation: VisualTransformation = VisualTransformation.None,
     keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-    onTextChange: (String) -> Unit
+    onTextChange: (String) -> Unit,
+    leadingIcon: @Composable () -> Unit = {},
 ) {
     var textVal by remember { mutableStateOf(defaultText) }
     val colors = TextFieldDefaults.textFieldColors(
@@ -74,6 +77,7 @@ private fun MySootheTextField(
         modifier = modifier
             .height(56.dp)
             .fillMaxWidth(),
+        leadingIcon = leadingIcon,
         textStyle = MaterialTheme.typography.body1,
         placeholder = { TextBody1(text = label) },
         value = textVal, onValueChange = { textVal = it; onTextChange(it) },
@@ -90,6 +94,17 @@ fun MySootheEmail(modifier: Modifier = Modifier, label: String, defaultText: Str
             .height(56.dp)
             .fillMaxWidth(),
         label = label,
+        onTextChange = { onTextChange(it) }
+    )
+}
+
+@Composable
+fun MySootheSearch(modifier: Modifier = Modifier, label: String, defaultText: String = "", onTextChange: (String) -> Unit) {
+    val search = painterResource(id = R.drawable.ic_search)
+    MySootheTextField(
+        modifier = modifier.height(56.dp).fillMaxWidth(),
+        label = label,
+        leadingIcon = { Icon(search, "Search icon", modifier = Modifier.size(18.dp)) },
         onTextChange = { onTextChange(it) }
     )
 }
