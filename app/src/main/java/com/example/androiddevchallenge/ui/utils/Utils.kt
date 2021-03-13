eaad80b713b93dbd1f71a2fbbfb30e909b671e9e
Miquel Àngel <miquelrc95@outlook.es>
2021-03-13 12:28:53 +0100

    Fixes background

--- app/src/main/java/com/example/androiddevchallenge/ui/utils/Utils.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/utils/Utils.kt
@@ -2,21 +2,23 @@ package com.example.androiddevchallenge.ui.utils
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 
 
 @Composable
-fun BackgroundImage(@DrawableRes backgroundImageId: Int, content: @Composable () -> Unit) {
+fun BackgroundImage(@DrawableRes backgroundImageId: Int, modifier: Modifier = Modifier,content: @Composable () -> Unit) {
     val bgPainter = painterResource(id = backgroundImageId)
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize().background(MaterialTheme.colors.background)
     ) {
-        Image(modifier = Modifier.fillMaxSize(), painter = bgPainter, contentDescription = "Background of Welcome screen")
+        Image(modifier = modifier.fillMaxSize(), painter = bgPainter, contentDescription = "Background of Welcome screen", contentScale = ContentScale.FillBounds)
         content()
     }
 }
