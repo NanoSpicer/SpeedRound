398aa46acb4c7f13019689e001a6de6583e0fed5
Miquel Àngel <miquelrc95@outlook.es>
2021-03-13 11:14:02 +0100

    Configure typographies

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -15,29 +15,72 @@
  */
 package com.example.androiddevchallenge.ui.theme
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.toFontFamily
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
+import java.util.*
 
+
+val kulimParkLight = Font(R.font.kulimpark_light).toFontFamily()
+val kulimParkRegular = Font(R.font.kulimpark_regular).toFontFamily()
+val latoBold = Font(R.font.lato_bold).toFontFamily()
+val latoRegular = Font(R.font.lato_regular).toFontFamily()
 // Set of Material typography styles to start with
 val typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = kulimParkLight,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 28.sp,
+        letterSpacing = 1.15.sp
+    ),
+    // CAPS
+    h2 = TextStyle(
+        fontFamily = kulimParkRegular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        letterSpacing = 1.15.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = latoBold,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = latoRegular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
     ),
     caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        letterSpacing = 1.15.sp,
+    ),
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        letterSpacing = 1.15.sp,
     )
-    */
 )
+
+@Composable fun TextH1(modifier: Modifier = Modifier, text: String) = Text(modifier = modifier, text = text, style = MaterialTheme.typography.h1)
+@Composable fun TextH2(modifier: Modifier = Modifier, text: String) = Text(modifier = modifier, text = text.CAPS, style = MaterialTheme.typography.h2)
+@Composable fun TextH3(modifier: Modifier = Modifier, text: String) = Text(modifier = modifier, text = text, style = MaterialTheme.typography.h3)
+@Composable fun TextBody1(modifier: Modifier = Modifier, text: String) = Text(modifier = modifier, text = text, style = MaterialTheme.typography.body1)
+@Composable fun TextCaption(modifier: Modifier = Modifier, text: String) = Text(modifier = modifier, text = text.CAPS, style = MaterialTheme.typography.caption)
+@Composable fun TextButton(modifier: Modifier = Modifier, text: String) = Text(modifier = modifier, text = text.CAPS, style = MaterialTheme.typography.button)
+
+private val String.CAPS get() = toUpperCase(Locale.ROOT)
+
