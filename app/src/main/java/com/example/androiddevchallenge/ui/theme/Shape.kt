dc6524683362bf80d975e5ced23fbc10a6d84717
Miquel Àngel <miquelrc95@outlook.es>
2021-03-13 11:13:47 +0100

    Fix shapes, add spacing

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Shape.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Shape.kt
@@ -20,6 +20,6 @@ import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val shapes = Shapes(
-    small = RoundedCornerShape( 0.dp, 4.dp, 0.dp, 0.dp),
-    medium = RoundedCornerShape(0.dp, 16.dp, 0.dp, 0.dp),
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(16.dp),
 )
---
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Spacing.kt
@@ -0,0 +1,8 @@
+package com.example.androiddevchallenge.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+
+val smSpacing = 8.dp
+val mdSpacing = 16.dp
+val lgSpacing = 32.dp
